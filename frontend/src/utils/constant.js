0d0b392a0ef976952eb0a8f94d7a13c2a016b1dd
Prashant12301 <prashantyadav12301@gmail.com>
2025-12-07 21:58:13 +0530

    updated

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,7 @@
-export const USER_API_END_POINT="https://job-portal-app-backend-yu4k.onrender.com/api/v1/user";
-export const JOB_API_END_POINT="https://job-portal-app-backend-yu4k.onrender.com/api/v1/job";
-export const APPLICATION_API_END_POINT="https://job-portal-app-backend-yu4k.onrender.com/api/v1/application";
-export const COMPANY_API_END_POINT="https://job-portal-app-backend-yu4k.onrender.com/api/v1/company";
+const API_URL = import.meta.env.VITE_API_URL;
+export const USER_API_END_POINT = `${API_URL}/api/v1/user`;
+export const COMPANY_API_END_POINT = `${API_URL}/api/v1/company`;
+export const JOB_API_END_POINT = `${API_URL}/api/v1/job`;
+export const APPLICATION_API_END_POINT = `${API_URL}/api/v1/application`;
+
+
